81580ccfa5e0c59de20075009900f4f9db9bfc82
Lycoris69 <simeon.petrou@epitech.eu>
2026-06-11 19:18:47 +0200

    chore: release 2.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.lycoris.noboundrift"
         minSdk = 26
         targetSdk = 35
-        versionCode = 44
-        versionName = "2.0.5"
+        versionCode = 45
+        versionName = "2.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
