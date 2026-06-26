4d6e7f12498b3281f8b60d70ecc6734ff4647e61
Lycoris69 <simeon.petrou@epitech.eu>
2026-06-26 13:25:00 +0200

    chore: bump version to v2.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.lycoris.noboundrift"
         minSdk = 26
         targetSdk = 35
-        versionCode = 61
-        versionName = "2.1.9"
+        versionCode = 62
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
